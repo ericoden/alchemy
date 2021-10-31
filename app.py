bb65c5cf8c6ef720846c81c6133e3ab0b65d0ef6
Eric Oden <ericjoden94@gmail.com>
2021-10-30 22:13:19 -0400

    add better UI to app

--- app.py
+++ app.py
@@ -12,15 +12,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def my_form():
-    return render_template('my-form.html', ingredients = ingredients)
+    z = -1
+    inventory = [0 for i in ingredients]
+    inventory_df = pd.DataFrame({'ingredient':ingredients, 'inventory':inventory})
+    return render_template('my-form.html', inventory_df=inventory_df, z = z, df=[])
 
 @app.route('/', methods=['POST'])
 def my_form_post():
     inventory = []
     for i in ingredients:
         inventory.append(int(request.form[i]))
+    inventory_df = pd.DataFrame({'ingredient':ingredients,'inventory':inventory})
     [z, df] = create_model(inventory)
-    return 'Optimal Objective: ' + z
+    indices = [i for i in range(len(df))]
+    names = df['name']
+    counts = df['count']
+    values = df['value']
+    first_ingredient = df['first_ingredient']
+    second_ingredient = df['second_ingredient']
+    third_ingredient = df['third_ingredient']
+    descriptions = df['descriptions']
+    return render_template('my-form.html', inventory_df=inventory_df, ingredients = ingredients, z=z, df=df)
 
 if __name__ == "__main__":
     app.run(debug=True)
